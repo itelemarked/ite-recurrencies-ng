0d11fd7f01188415001c0fa9277befc590696545
itelemarked <me@you.com>
2024-09-19 10:24:23 +0200

    testing switchmap vs combineLatest

--- src/app/test/test.page.ts
+++ src/app/test/test.page.ts
@@ -1,8 +1,8 @@
-import { Component, effect, inject } from '@angular/core';
+import { Component, effect, ElementRef, inject, OnInit, signal, viewChild } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { FormsModule } from '@angular/forms';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons, IonModal, IonDatetime, IonInput } from '@ionic/angular/standalone';
-import { map, Observable, of, switchMap, tap } from 'rxjs';
+import { combineLatestWith, delay, fromEvent, interval, map, Observable, of, switchMap, take, tap } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
 import { toObservable } from '@angular/core/rxjs-interop'
 import { AngularFireAuth } from '@angular/fire/compat/auth';
@@ -23,6 +23,7 @@ import { ISettings } from '../settings/settings.interface';
     <ion-content [forceOverscroll]="false">
 
       <p>TestPage works</p>
+      <ion-button #btn (click)="clickCounter.set(clickCounter() + 1)">Click me</ion-button>
     
     </ion-content>
   `,
@@ -30,58 +31,34 @@ import { ISettings } from '../settings/settings.interface';
 })
 export class TestPage {
 
-  private firestore = inject(AngularFirestore)
-  private auth = inject(AngularFireAuth)
-  private authService = inject(AuthService)
-  private settingsService = inject(SettingsService)
+  btn = viewChild('btn', {read: ElementRef})
+  clickCounter = signal(-1)
+  obs2$ = toObservable(this.clickCounter).pipe(map(val => `From obs2$: ${val}`))
 
-  constructor() {
-    effect(() => console.log(this.settingsService.get$()()))
-  }
+  ngOnInit() {
+    const obs1$ = interval(8000).pipe(map(val => `From obs1$: ${val}`))
+    // const obs2$ = fromEvent(this.btn()?.nativeElement, 'click').pipe(map(_ => `  From obs2$: clicked`))
+    const obs2$ = this.obs2$
+    const obs3$ = interval(1000).pipe(delay(8100), map(val => `From obs3$: ${val}`))
 
-  getPath$(): Observable<string | undefined> {
-    const user$ = toObservable(this.authService.userSig)
-    return user$.pipe(
-      map(usr => !!usr ? `users/${usr.id()}/settings/0`: undefined)
+    // const switch$ = obs1$.pipe(
+    //   tap(console.log),
+    //   switchMap(_ => obs2$),
+    //   tap(console.log),
+    //   switchMap(_ => obs3$),
+    // )
+
+    const combine$ = obs1$.pipe(
+      tap(_ => console.log(`--- obs$1 fired! ---`)),
+      combineLatestWith(obs2$.pipe(tap(_ => console.log(`--- obs$2 fired! ---`)) )),
+      map( ([e1, e2]) => [e1, e2]),
+      combineLatestWith(obs3$),
+      map( ([e12, e3]) => [...e12, e3])
     )
+
+    // obs2$.subscribe(console.log)
+    // switch$.pipe(take(10)).subscribe(console.log)
+    combine$.pipe(take(20)).subscribe( ([e1, e2, e3]) => console.log(`${e3}, [${e1}, ${e2}]`))
   }
-  
-
-  // constructor() {
-  //   const path$ = toSignal(this.getPath$(), {initialValue: undefined})
-  //   const store = new FirebaseDocumentStore(path$)
-  //   store.get$<ISettings>().subscribe(console.log)
-  // }
-
-  // getPath$(): Observable<string | undefined> {
-  //   const user$ = toObservable(this.authService.userSig)
-  //   return user$.pipe(
-  //     map(usr => !!usr ? `users/${usr.id()}/settings/0`: undefined)
-  //   )
-  // }
-
-  getSettings$(): Observable<ISettings | undefined> {
-    const path$ = this.getPath$()
-    return path$.pipe(
-      switchMap(path => {
-        if(!!path) {
-          return this.firestore.doc<ISettings>(path).valueChanges()
-        }
-        return of(undefined)
-      })
-    )
-  }
-
-  // getSettings$(): Observable<ISettings | undefined> {
-  //   const user$ = toObservable(this.authService.userSig)
-  //   return user$.pipe(
-  //     switchMap(usr => {
-  //       if(!!usr) {
-  //         return this.firestore.doc<ISettings>(`users/${usr.id()}/settings/0`).valueChanges()
-  //       }
-  //       return of(undefined)
-  //     })
-  //   )
-  // }
 
 }
