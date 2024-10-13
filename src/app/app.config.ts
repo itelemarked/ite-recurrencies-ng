cab43fd1b4a0972c1a58c655668e8794133fa0a8
itelemarked <itelemarked@icloud.com>
2024-10-13 21:31:27 +0200

    ngrx/store installed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideIonicAngular } from '@ionic/angular/standalone';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideIonicAngular({})]
+  providers: [
+    provideRouter(routes),
+    provideIonicAngular({}),
+    provideStore()
+  ]
 };
