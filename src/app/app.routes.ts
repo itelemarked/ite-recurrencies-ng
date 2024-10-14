91bd0e9be90699b4bd3963fb99c4b5d1903e13bb
itelemarked <itelemarked@icloud.com>
2024-10-14 20:57:32 +0200

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './modules/home/home.page';
-import { RecurrencyPage } from './modules/recurrency/recurrency.page';
-import { recurrenciesRoutes } from './modules/recurrency/recurrency.routes'
 import { TestPage } from './modules/test/test.page';
 import { AuthPage } from './modules/auth/auth.page';
 
@@ -11,7 +9,6 @@ export const routes: Routes = [
   { path: '', redirectTo: ORIGIN, pathMatch: 'full' },
 
   { path: 'home', component: HomePage },
-  { path: 'recurrencies', component: RecurrencyPage, children: recurrenciesRoutes },
   { path: 'auth', component: AuthPage },
   { path: 'test', component: TestPage },
 
--- src/app/modules/test/test.page.ts
+++ src/app/modules/test/test.page.ts
@@ -1,11 +1,7 @@
-import { Component, computed, OnInit, signal } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons, IonModal, IonDatetime, IonInput } from '@ionic/angular/standalone';
-import { Recurrency } from '../recurrency/Recurrency';
-import { BehaviorSubject, of } from 'rxjs';
-import { RecurrencyService } from '../recurrency/recurrency.service';
-import { AuthService } from '../auth/auth.service';
+
 
 @Component({
   selector: 'app-test',
@@ -21,69 +17,13 @@ import { AuthService } from '../auth/auth.service';
     <ion-content [forceOverscroll]="false">
 
       <p>TestPage works</p>
-      <p>value: {{ inputValue }}</p>
-      <ion-button (click)="isModalOpen = true">open modal</ion-button>
-      <p>Counter: {{ counter() }}</p>
-
-      <ion-modal [isOpen]="isModalOpen">
-        <ng-template>
-          <ion-header>
-            <ion-toolbar>
-              <ion-title>Modal</ion-title>
-            </ion-toolbar>
-          </ion-header>
-          <ion-content class="ion-padding">
-            <ion-input [(ngModel)]="inputValue"></ion-input>
-            <ion-button (click)="isModalOpen = false">ok</ion-button>
-          </ion-content>
-        </ng-template>
-      </ion-modal>
     
     </ion-content>
   `,
   styles: ``,
 })
-export class TestPage implements OnInit {
+export class TestPage {
 
-  isModalOpen = false
-  inputValue = 99
-
-  settings = signal({
-    count: 999
-  })
-
-  counter = computed(() => this.settings().count)
-
-  _user$ = new BehaviorSubject({id: '0yuA0RLZFJdbRKtVSfW4y5HSQMq'})
-  user$ = this._user$.asObservable()
-
-  constructor(
-    private recService: RecurrencyService,
-    private auth: AuthService
-  ) {
-    // this.recService.get$(this.user$).subscribe(console.log)
-
-    // setTimeout(() => {
-    //   this._user$.next({id: '0yuA0RLZFJdbRKtVSfW4y5HSQMq1'})
-    // }, 3000);
   
-    // const newRec = {
-    //   title: 'new Recurrency 6',
-    //   lastEvent: '2000-01-01',
-    //   periodNb: 99,
-    //   periodUnit: 'daaays'
-    // }
-
-    // this.auth.login('aaa@aaa.com', '111111')
-    // .then(() => console.log('logged-in!'))
-    // .catch(() => console.log('mmmmh, some error here...'))
-    // this.auth.user$.subscribe(console.log)
-
-    // this.store.delete('users/0yuA0RLZFJdbRKtVSfW4y5HSQMq1/recurrencies/iBHVgbuDm9NnfY1wAEjg')
-  }
-
-  ngOnInit(): void {
-    
-  }
 
 }
