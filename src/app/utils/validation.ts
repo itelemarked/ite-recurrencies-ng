3e0a217f2d74655f9db6434ca266cefd60999d14
itelemarked <itelemarked@icloud.com>
2025-07-29 22:43:00 +0200

    utils validation added

--- src/app/utils/validation.ts
+++ src/app/utils/validation.ts
@@ -1,71 +1,75 @@
 
-export function isNull(val: any): val is null {
-  return val === null
-}
+export const isUndefined = (val: any): val is undefined => val === undefined
 
-export function isUndefined(val: any): val is undefined {
-  return val === undefined
-}
-export function isString(val: any): val is string {
-  return typeof val === 'string'
-}
+export const isNull = (val: any): val is null => val === null
 
-export function isNumber(val: any): val is number {
-  return typeof val === 'number'
-}
+export const isString = (val: any): val is string => typeof val === 'string'
 
-export function isBoolean(val: any): val is boolean {
-  return typeof val === 'boolean'
-}
+export const isNumber = (val: any): val is number => typeof val === 'number'
 
-export function isArray(val: any): val is any[] {
-  return Array.isArray(val)
-}
+export const isBoolean = (val: any): val is boolean => typeof val === 'boolean'
 
-export function isOptional(val: any): val is any[] {
-  return true
-}
+export const isArray = <T>(val: any): val is T[] => Array.isArray(val)
+
+export const isPlainObject = (val: any): val is Record<string, any> => val !== null && val !== undefined && Object.getPrototypeOf(val) === Object.prototype
+
+export const isFunction = (val: any): val is ((...args: any) => any) => typeof val === 'function'
+
+
 
 /**
- * 
- * Use 'isObject' to validate an interface.
- * The second argument is map object, with an array of any built in validators (see above) or any function of type (val: any) => boolean.
- * 
- * How to use:
- * 
- *  type Point = {
- *    x: DateString,
- *    y: number,
- *    z?: string
- *  }
- *
- *  
- *  isObject(p, {
- *    x: [(val: any) => isDateStringForTimezone(val, 'Europe/Zurich')],
- *    y: [isNumber],
- *    z: [isString, isOptional]
- *  })
+ * THE ONLY PURPOSE OF 'isOpional' IS TO BE PASSED AS 'fns' ARGUMENT TO THE 'isInterface()' FUNCTION.
+ * IT IS NO USE TO USE IT AS IS. 
  */
-export function isObject<T extends Record<string, unknown>>(val: any, options?: Record<string, (((...args: any) => boolean))[]>): val is T {
-  const isPlainObj = (val: any): val is Record<string, unknown> => val !== null && val !== undefined && Object.getPrototypeOf(val) === Object.prototype
-  if(!isPlainObj(val)) return false
-  if(options === undefined) return true
+export const isOptional = (val: any) => true
+
+/**
+ * Example of use:
+ * 
+ * Given the following interface 'Graph' and a 'graph1' variable: 
+ * 
+ *  type Graph = {
+ *    xLabel: string, 
+ *    yLabel: string, 
+ *    origin: {
+ *      x: number, 
+ *      y: number
+ *    },
+ *    description?: string
+ *  }
+ * 
+ *  const graph1 = {
+ *    xLabel: 'time',
+ *    yLabel: 'distance',
+ *    origin: {
+ *      x: 10,
+ *      y: 10
+ *    }
+ *  }
+ * 
+ *  const interfaceGraph = {
+ *    xLabel: [isString],
+ *    yLabel: [isString],
+ *    origin: [
+ *      isInterface({
+ *        x: [isNumber],
+ *        y: [isNumber]
+ *      })
+ *    ],
+ *    description: [isString, isOptional]
+ *  }
+ * 
+ *  console.log(isInterface<Graph>(interfaceGraph)(graph1))
+ */
+export const isInterface = <T extends Record<string, any>>(fns: Record<string, ((arg: any) => boolean)[]>) => (val: any): val is T => {
+  // CHECKS 'val' IS A 'PlainObject'
+  if(!isPlainObject(val)) return false
+
+  // CHECKS 'VAL' HAS ALL REQUIRED KEYS
+  const optionalKeys = Object.entries(fns).filter(([key, values]) => values.map(v => v.name).includes('isOptional')).map(([key, values]) => key)
+  const requiredKeys = Object.keys(fns).filter(key => !optionalKeys.includes(key))
+  if(!requiredKeys.every(requiredKey => Object.keys(val).includes(requiredKey))) return false
   
-  return Object.entries(options).every(([prop, fns]) => {
-
-    // no property with isOptional option
-    if(!(prop in val) && fns.map(fn => fn.name).includes('isOptional')) {
-      return true
-    }
-
-    // property exists and is undefined, with isOptional option
-    if(prop in val && isUndefined(val[prop])) {
-      return true
-    }
-    
-    // Otherwise, property must exist and be valid for every function
-    return (prop in val) && fns.every(fn => fn(val[prop]))
-
-  })
+  // CHECKS EVERY PROPS OF 'val' FULLFILLS THE REQUIRED CONDITIONS ('isOptional' IS REMOVED FROM THE REQUIRED CONDITIONS LIST)
+  return Object.entries(val).every(([key, value]) => fns[key].filter(fn => fn.name !== 'isOptional').some(fn => fn(value)))
 }
-
