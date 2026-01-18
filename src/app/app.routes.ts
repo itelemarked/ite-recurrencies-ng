f3b56d8e824d1c4a16051eab18843c9cc5e0657f
itelemarked <me@you.com>
2026-01-18 23:26:21 +0100

    testing feature added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { RecurrencyListPage } from './recurrencies/recurrency-list.page';
+import { TestingPage } from './testing/testing.page';
 
 
 const ORIGIN = '/recurrencies'
@@ -7,6 +8,7 @@ const ORIGIN = '/recurrencies'
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: ORIGIN},
   { path: 'recurrencies', component: RecurrencyListPage },
+  { path: 'testing', component: TestingPage },
   // { path: 'brb', component: BrbListPage },
   // { path: 'settings', component: SettingsMainPage },
   // { path: 'testing', component: TestingPage },
---
+++ src/app/testing/testing.page.ts
@@ -0,0 +1,39 @@
+
+import { Component } from "@angular/core";
+import { IonContent, IonHeader, IonTitle, IonToolbar } from "@ionic/angular/standalone";
+
+@Component({
+  selector: 'app-testing-page',
+  standalone: true,
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+  ],
+  template: `
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>
+          Testing
+        </ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content [forceOverscroll]="false">
+      TestingPage works!
+    </ion-content>
+  `,
+  styles: [``]
+})
+export class TestingPage {
+  // DEPENDENCIES
+
+  // STATE
+
+  // SELECTORS
+
+  // ACTIONS
+
+  // PRIVATE
+
+}
