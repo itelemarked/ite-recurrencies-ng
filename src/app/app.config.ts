ad9472c230c5c70ab04e1a7649c43f9407ae542b
itelemarked <itelemarked@itelemarked.com>
2026-01-25 22:20:20 +0100

    commit (angular 21 not working... reinstall)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,36 +1,46 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideIonicAngular } from '@ionic/angular/standalone';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { getDatabase, provideDatabase } from '@angular/fire/database';
 
-import { FIREBASE_OPTIONS } from '@angular/fire/compat';
+// import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+// import { getAuth, provideAuth } from '@angular/fire/auth';
+// import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+// import { getDatabase, provideDatabase } from '@angular/fire/database';
+
+// import { FIREBASE_OPTIONS } from '@angular/fire/compat';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCH5hQQ-umEKcyfISQJ-JaM8Bi22Q3VYlk",
-  authDomain: "ite-recurrencies.firebaseapp.com",
-  projectId: "ite-recurrencies",
-  storageBucket: "ite-recurrencies.firebasestorage.app",
-  messagingSenderId: "202558768735",
-  appId: "1:202558768735:web:fd080d6a7c97259c687dea"
+  apiKey: 'AIzaSyCH5hQQ-umEKcyfISQJ-JaM8Bi22Q3VYlk',
+  authDomain: 'ite-recurrencies.firebaseapp.com',
+  projectId: 'ite-recurrencies',
+  storageBucket: 'ite-recurrencies.firebasestorage.app',
+  messagingSenderId: '202558768735',
+  appId: '1:202558768735:web:fd080d6a7c97259c687dea',
 };
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideIonicAngular({
-      useSetInputAPI: true
+      useSetInputAPI: true,
     }),
-    { provide: FIREBASE_OPTIONS, useValue: firebaseConfig }, // Needed for using angularfire compat API
-    provideFirebaseApp(() => initializeApp(firebaseConfig)), 
-    provideAuth(() => getAuth()), 
-    provideFirestore(() => getFirestore()), 
-    provideDatabase(() => getDatabase()), provideFirebaseApp(() => initializeApp({ projectId: "ite-recurrencies", appId: "1:202558768735:web:fd080d6a7c97259c687dea", storageBucket: "ite-recurrencies.firebasestorage.app", apiKey: "AIzaSyCH5hQQ-umEKcyfISQJ-JaM8Bi22Q3VYlk", authDomain: "ite-recurrencies.firebaseapp.com", messagingSenderId: "202558768735", projectNumber: "202558768735", version: "2" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideFirebaseApp(() => initializeApp({ projectId: "ite-recurrencies", appId: "1:202558768735:web:fd080d6a7c97259c687dea", storageBucket: "ite-recurrencies.firebasestorage.app", apiKey: "AIzaSyCH5hQQ-umEKcyfISQJ-JaM8Bi22Q3VYlk", authDomain: "ite-recurrencies.firebaseapp.com", messagingSenderId: "202558768735", projectNumber: "202558768735", version: "2" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase())
-  ]
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    // { provide: FIREBASE_OPTIONS, useValue: firebaseConfig }, // Needed for using angularfire compat API
+    // provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    // provideAuth(() => getAuth()),
+    // provideFirestore(() => getFirestore()),
+    // provideDatabase(() => getDatabase()),
+    // provideFirebaseApp(() =>
+    //   initializeApp({
+    //     apiKey: 'AIzaSyCH5hQQ-umEKcyfISQJ-JaM8Bi22Q3VYlk',
+    //     authDomain: 'ite-recurrencies.firebaseapp.com',
+    //     projectId: 'ite-recurrencies',
+    //     storageBucket: 'ite-recurrencies.firebasestorage.app',
+    //     messagingSenderId: '202558768735',
+    //     appId: '1:202558768735:web:fd080d6a7c97259c687dea',
+    //   }),
+    // ),
+  ],
 };
-
-
--- src/app/recurrencies/services/recurrency.service.ts
+++ src/app/recurrencies/services/recurrency.service.ts
@@ -1,9 +1,10 @@
 import { computed, inject, Injectable } from "@angular/core";
 import { BehaviorSubject } from "rxjs";
-import { toSignal } from "@angular/core/rxjs-interop";
 import { isRecurrencyData, Recurrency, RecurrencyData } from "../types/Recurrency.type";
 import { isPlainObject } from "src/js/valid-type";
 import { SettingsService } from "@app/settings/settings.service";
+
+
 
 @Injectable({providedIn: 'root'})
 export class RecurrencyService {
@@ -28,7 +29,7 @@ export class RecurrencyService {
 
   recurrencies$ = this.state.recurrencies$.asObservable()
 
-  recurrencies = toSignal(this.recurrencies$, {requireSync: true})
+  recurrencies = toSignal (this.recurrencies$, {requireSync: true})
 
   set(recurrencies: Recurrency[]): Promise<void> {
     this.state.recurrencies$.next(recurrencies)
